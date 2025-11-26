5565cccef541845d5b14aef31abcbc73e88906ec
Hegui Dai <natural_selection_@outlook.com>
2025-11-26 16:54:52 +0800

    init repo

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="flex flex-col h-screen w-screen overflow-hidden bg-sidebar">
+        </div>
+    );
+}
+
+export default App;
