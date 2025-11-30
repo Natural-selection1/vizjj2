0f32ad1ef9365b4d35c28ab9544c7693e8774d3c
Hegui Dai <natural_selection_@outlook.com>
2025-11-30 12:34:12 +0800

    feat(UI): init resizable area

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
 import TitleBar from "./components/title_bar";
 import "./App.css";
 
@@ -7,6 +8,48 @@ function App() {
             {/* 1. Titlebar */}
             <div className="h-[33px] w-full shrink-0 border-b border-border">
                 <TitleBar />
+            </div>
+
+            {/* 2. Operation Bar */}
+            <div className="h-[70px] w-full shrink-0 bg-sidebar text-sidebar-foreground border-b border-sidebar-border flex items-center justify-center">
+                <span className="font-bold">Operation Bar</span>
+            </div>
+
+            {/* 3. Middle Area */}
+            <div className="flex-1 w-full min-h-0">
+                <ResizablePanelGroup direction="horizontal" className="h-full w-full">
+                    {/* Left Info Panel */}
+                    <ResizablePanel
+                        defaultSize={15}
+                        minSize={5}
+                        className="bg-sidebar text-sidebar-foreground flex items-center justify-center">
+                        <span className="font-bold">Left Info</span>
+                    </ResizablePanel>
+
+                    {/* Resize Handle */}
+                    <ResizableHandle className="bg-border hover:bg-sidebar transition-colors" />
+
+                    {/* Main Content Panel */}
+                    <ResizablePanel
+                        defaultSize={60}
+                        minSize={20}
+                        className="bg-background text-foreground flex flex-col"></ResizablePanel>
+
+                    <ResizableHandle className="bg-border hover:bg-sidebar transition-colors" />
+
+                    {/* Right Info Panel */}
+                    <ResizablePanel
+                        defaultSize={25}
+                        minSize={5}
+                        className="bg-sidebar text-sidebar-foreground flex items-center justify-center">
+                        <span className="font-bold">Right Info</span>
+                    </ResizablePanel>
+                </ResizablePanelGroup>
+            </div>
+
+            {/* 4. Bottom Bar */}
+            <div className="h-[35px] w-full shrink-0 bg-sidebar text-sidebar-foreground flex items-center justify-center">
+                <span className="font-bold">Bottom Bar</span>
             </div>
         </div>
     );
